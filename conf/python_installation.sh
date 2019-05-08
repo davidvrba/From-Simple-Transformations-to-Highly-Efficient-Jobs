557fcfb78294dc1307f807b77949f44e8e2e55d0
David Vrba <david.vrba@socialbakers.com>
2019-05-08 08:02:01 +0200

    assume-yes added to installation script

--- conf/python_installation.sh
+++ conf/python_installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get  update
-apt install python3-pip
-apt-get install openjdk-8-jdk
+apt --assume-yes install python3-pip
+apt-get --assume-yes install openjdk-8-jdk
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
-apt-get install scala
+apt-get --assume-yes install scala
 pip3 install py4j
 pip3 install numpy
 pip3 install pandas
@@ -15,13 +15,15 @@ pip3 install jupyter
 wget /home/david apache.miloslavbrada.cz/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz
 tar xvf spark-2.4.3-bin-hadoop2.7.tgz
 
+cp /home/david/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/hive-site.xml /home/david/spark-2.4.3-bin-hadoop2.7/conf/
+
 echo 'export SPARK_HOME=/home/david/spark-2.4.3-bin-hadoop2.7' >> ~/.bashrc
 echo 'export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH' >> ~/.bashrc
 echo 'export PYSPARK_PYTHON=python3' >> ~/.bashrc
 
 wget -P /home/david/spark-2.4.3-bin-hadoop2.7/jars https://jdbc.postgresql.org/download/postgresql-9.4.1212.jar
 
-apt install postgresql postgresql-contrib
+apt --assume-yes install postgresql postgresql-contrib
 sudo -u postgres bash -c "psql -c \"CREATE DATABASE metastore; \""
 sudo -u postgres bash -c "psql -c \"CREATE USER hiveuser with encrypted password 'hiveuser'; \""
 sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES on database metastore to hiveuser; \""
