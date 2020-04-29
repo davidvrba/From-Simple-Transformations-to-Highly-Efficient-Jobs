169f16c0ef1090f6e5e49cebd5053d32d5d9549e
David Vrba <david.vrba@vrbad.local>
2020-04-29 21:54:51 +0200

    migrate to spark 3.0.0 preview2

--- conf/python_installation.sh
+++ conf/python_installation.sh
@@ -10,7 +10,7 @@ apt-get --assume-yes install scala
 pip3 install py4j
 pip3 install numpy
 pip3 install pandas
-pip3 install pyarrow
+pip3 install 'pyarrow==0.16.0'
 pip3 install scipy
 pip3 install matplotlib
 pip3 install geopy
@@ -18,21 +18,19 @@ pip3 install geopandas
 pip3 install descartes
 pip3 install jupyter
 
-wget /home/$student https://archive.apache.org/dist/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
 wget /home/$student https://archive.apache.org/dist/spark/spark-3.0.0-preview2/spark-3.0.0-preview2-bin-hadoop2.7.tgz
 tar xvf spark-2.4.5-bin-hadoop2.7.tgz
 tar xvf spark-3.0.0-preview2-bin-hadoop2.7.tgz
 
-cp /home/$student/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/hive-site.xml /home/$student/spark-2.4.5-bin-hadoop2.7/conf/
 cp /home/$student/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/hive-site.xml /home/$student/spark-3.0.0-preview2-bin-hadoop2.7/conf/
 
-echo 'export SPARK_HOME=/home/'$student'/spark-2.4.5-bin-hadoop2.7' >> ~/.bashrc
+echo 'export SPARK_HOME=/home/'$student'/spark-3.0.0-preview2-bin-hadoop2.7' >> ~/.bashrc
 echo 'export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH' >> ~/.bashrc
 echo 'export PYSPARK_PYTHON=python3' >> ~/.bashrc
 
-SPARK_HOME=/home/$student/spark-2.4.5-bin-hadoop2.7
+SPARK_HOME=/home/$student/spark-3.0.0-preview2-bin-hadoop2.7
 
-wget -P /home/$student/spark-2.4.5-bin-hadoop2.7/jars https://jdbc.postgresql.org/download/postgresql-9.4.1212.jar
+wget -P /home/$student/spark-3.0.0-preview2-bin-hadoop2.7/jars https://jdbc.postgresql.org/download/postgresql-9.4.1212.jar
 
 apt --assume-yes install postgresql postgresql-contrib
 sudo -u postgres bash -c "psql -c \"CREATE DATABASE metastore; \""
@@ -43,8 +41,6 @@ sudo -u postgres bash -c "psql -c \" create table locations(name varchar, latitu
 sudo -u postgres bash -c "psql -c \" COPY locations FROM '/home/$student/From-Simple-Transformations-to-Highly-Efficient-Jobs/data/locations.csv' DELIMITERS ',' CSV HEADER; \""
 sudo -u postgres bash -c "psql -c \" create table user_clusters(user_id bigint, cluster_id integer); \""
 sudo -u postgres bash -c "psql -c \"ALTER USER postgres PASSWORD 'postgres'; \" "
-
-chmod +x From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/spark-cli.sh
 
 source ~/.bashrc
 
@@ -65,4 +61,6 @@ cat <<EOF >/home/ubuntu/.jupyter/jupyter_notebook_config.json
 EOF
 echo 'export PUBLIC_DNS='$2 >> ~/.bashrc
 echo "To run jupyter call: jupyter notebook --no-browser --port=8080 --ip=PUBLIC_DNS"
+echo "sudo chmod 777 .jupyter"
+echo "source .bashrc"
 fi
