4ff8cf182f9c9180cdd6f2e05b4e81e4ff6e0658
vrbad <david.vrba@emplifi.io>
2024-07-28 21:15:25 +0200

    spark 3.5 update

--- conf/python_installation.sh
+++ conf/python_installation.sh
@@ -3,7 +3,6 @@
 student=$1
 
 apt-get  update
-apt install python3.7
 apt --assume-yes install python3-pip
 apt-get --assume-yes install openjdk-8-jdk
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
@@ -11,27 +10,26 @@ apt-get --assume-yes install scala
 pip3 install py4j
 pip3 install numpy
 pip3 install pandas
-pip3 install 'pyarrow==1.0.0'
+pip3 install pyarrow
 pip3 install scipy
-pip3 install matplotlib==3.0.0
-pip3 install --upgrade cython
+pip3 install matplotlib
 pip3 install geopy
-pip3 install geopandas==0.9.0
+pip3 install geopandas
 pip3 install descartes
 pip3 install jupyter
 
-wget /home/$student https://archive.apache.org/dist/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz
-tar xvf spark-3.3.0-bin-hadoop3.tgz
+wget /home/$student https://archive.apache.org/dist/spark/spark-3.5.1/spark-3.5.1-bin-hadoop3.tgz
+tar xvf spark-3.5.1-bin-hadoop3.tgz
 
-cp /home/$student/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/hive-site.xml /home/$student/spark-3.3.0-bin-hadoop3/conf/
+cp /home/$student/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/hive-site.xml /home/$student/spark-3.5.1-bin-hadoop3/conf/
 
-SPARK_HOME=/home/$student/spark-3.3.0-bin-hadoop3
+SPARK_HOME=/home/$student/spark-3.5.1-bin-hadoop3
 
 echo 'export SPARK_HOME='$SPARK_HOME >> ~/.bashrc
 echo 'export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH' >> ~/.bashrc
 echo 'export PYSPARK_PYTHON=python3' >> ~/.bashrc
 
-wget -P /home/$student/spark-3.3.0-bin-hadoop3/jars https://jdbc.postgresql.org/download/postgresql-42.4.0.jar
+wget -P /home/$student/spark-3.5.1-bin-hadoop3/jars https://jdbc.postgresql.org/download/postgresql-42.4.0.jar
 source ~/.bashrc
 
 cd From-Simple-Transformations-to-Highly-Efficient-Jobs
--- installation-notes.md
+++ installation-notes.md
@@ -18,19 +18,12 @@
 * descartes
 * Postgresql
 
-### For Scala code:
-* java
-* scala
-* Apache Spark
-* Apache Zeppelin
-* Postgresql
-
-### On fresh Ubuntu 18.04 installation:
+### On fresh Ubuntu 22.04 installation:
 * There is an installation script that you can use for Python installation as follows (look inside conf/python_installation.sh to see what is being installed):
 ```
 sudo apt-get install git
 git clone https://github.com/davidvrba/From-Simple-Transformations-to-Highly-Efficient-Jobs.git
-chmod +x /home/student/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/python_installation.sh
-sudo /home/student/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/python_installation.sh student
+chmod +x /home/ubuntu/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/python_installation.sh
+sudo /home/ubuntu/From-Simple-Transformations-to-Highly-Efficient-Jobs/conf/python_installation.sh ubuntu
 ```
-* Note: It assumes that your home folder is /home/student. If it is not, you need to change the commands accordingly.
+* Note: It assumes that your home folder is /home/ubuntu. If it is not, you need to change the commands accordingly.
